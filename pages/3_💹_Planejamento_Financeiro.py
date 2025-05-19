5aae4fa81a510fd7f573b2e6a93f6c18cf2dc6f0
thalescarmo <thalesvrpc@gmail.com>
2025-05-19 17:18:31 -0300

    Update input formatting in Planejamento_Financeiro.py for improved user experience
    - Removed currency formatting from input fields for "Patrimônio Inicial," "Resgate Mensal," and "Aporte Mensal" to streamline data entry.
    - Ensured consistency in input formatting across all financial parameters for better clarity.

--- pages/3_💹_Planejamento_Financeiro.py
+++ pages/3_💹_Planejamento_Financeiro.py
@@ -14,10 +14,10 @@ st.title("Planejamento Financeiro")
 
 st.sidebar.header("Parâmetros")
 
-patrimonio_inicial = st.sidebar.number_input("Patrimônio Inicial (R$):", min_value=0, value=100000, step=10000, format=".0fR$")
+patrimonio_inicial = st.sidebar.number_input("Patrimônio Inicial (R$):", min_value=0, value=100000, step=10000, format=".0f")
 horizonte_temporal = st.sidebar.number_input("Horizonte Temporal (anos):", min_value=1, value=10, step=1, format=".0f")
-resgate_mensal = st.sidebar.number_input("Resgate Mensal (R$):", min_value=0, value=10000, step=1000, format=".0fR$")
-aporte_mensal = st.sidebar.number_input("Aporte Mensal (R$):", min_value=0, value=10000, step=1000, format=".0fR$")
+resgate_mensal = st.sidebar.number_input("Resgate Mensal (R$):", min_value=0, value=10000, step=1000, format=".0f")
+aporte_mensal = st.sidebar.number_input("Aporte Mensal (R$):", min_value=0, value=10000, step=1000, format=".0f")
 rentabilidade_anual_desejada = st.sidebar.number_input("Rentabilidade Anual Esperada (%):", min_value=0.0, value=10.0, step=0.1, format=".1f")
 inflacao_anual_desejada = st.sidebar.number_input("Inflação Anual Esperada (%):", min_value=0.0, value=2.0, step=0.1, format=".1f")
 aliquota_irrf = st.sidebar.number_input("Alíquota de Impostos (%):", min_value=0.0, value=15, step=0.1, format=".1f")
