a046c428334a8bb6865d293a76c2fad7390cc7bd
Thales Carmo <thales.carmo@persevera.com.br>
2025-11-10 18:58:58 -0300

    Refactor data aggregation and visualization logic in 3_📊_Distribuição_de_Posições.py to streamline position distribution analysis by asset class. Enhance error handling and improve user experience with clearer data presentation in the sidebar.

--- pages/3_💹_Distribuição_de_Posições.py
+++ pages/3_💹_Distribuição_de_Posições.py
@@ -58,33 +58,48 @@ with st.sidebar:
   selected_visualization = st.radio("Visualizar por", options=['Financeiro (R$)', 'Percentual da Classe (%)', 'Percentual do Total (%)'], index=0)
 
 if df is not None:
-    try:
-      # Composição Completa
-      df_positions = df.groupby([pd.Grouper(key='creation-date', freq='D'), 'Portfolio', 'Name', 'Ativo Nome Completo', 'Classificação do Conjunto']).agg(
-        **{
-          'Quantidade': ('Quantidade', 'sum'),
-          'Valor Unitário': ('Valor Unitário', 'mean'),
-          'Saldo': ('Saldo', 'sum')
-        }
+  try:
+    # Composição Completa
+    df_positions = df.groupby([pd.Grouper(key='creation-date', freq='D'), 'Portfolio', 'Name', 'Ativo Nome Completo', 'Classificação do Conjunto']).agg(
+      **{
+        'Quantidade': ('Quantidade', 'sum'),
+        'Valor Unitário': ('Valor Unitário', 'mean'),
+        'Saldo': ('Saldo', 'sum')
+      }
+    )
+    df_positions_current = df_positions.loc[df_positions.index.get_level_values(level=0).max()].reset_index()
+
+    # Saldo Total
+    df_total_positions = df.groupby([pd.Grouper(key='creation-date', freq='D'), 'Portfolio']).agg(**{'Saldo': ('Saldo', 'sum')})
+    df_total_positions_current = df_total_positions.loc[df_total_positions.index.get_level_values(level=0).max()]
+
+    df_total_positions_by_asset_class = df.groupby([pd.Grouper(key='creation-date', freq='D'), 'Portfolio', 'Classificação do Conjunto'])['Saldo'].sum()
+    df_total_positions_by_asset_class_current = df_total_positions_by_asset_class.loc[df_total_positions_by_asset_class.index.get_level_values(level=0).max()].reset_index()
+    df_total_positions_by_asset_class_current = df_total_positions_by_asset_class_current.pivot(index='Classificação do Conjunto', columns='Portfolio', values='Saldo')
+    df_total_positions_by_asset_class_current = df_total_positions_by_asset_class_current.reindex(asset_classes)
+    df_total_positions_by_asset_class_current = df_total_positions_by_asset_class_current.div(df_total_positions_by_asset_class_current.sum(axis=0), axis=1) * 100
+
+    st.markdown(f"##### Distribuição de Posições por Classe")
+    st.dataframe(
+      style_table(
+        df_total_positions_by_asset_class_current,
+        numeric_cols_format_as_float=list(df_total_positions_by_asset_class_current.columns),
       )
-      df_positions_current = df_positions.loc[df_positions.index.get_level_values(level=0).max()].reset_index()
+    )
 
-      # Saldo Total
-      df_total_positions = df.groupby([pd.Grouper(key='creation-date', freq='D'), 'Portfolio']).agg(**{'Saldo': ('Saldo', 'sum')})
-      df_total_positions_current = df_total_positions.loc[df_total_positions.index.get_level_values(level=0).max()]
+    # Posições por Ativo e Classe
+    for asset_class in asset_classes:
+      df_asset_class_positions = df_positions_current[df_positions_current['Classificação do Conjunto'] == asset_class]
+      df_asset_class_positions = df_asset_class_positions.pivot(index=['Name', 'Ativo Nome Completo'], columns='Portfolio', values='Saldo')
 
-      for asset_class in asset_classes:
-        df_asset_class_positions = df_positions_current[df_positions_current['Classificação do Conjunto'] == asset_class]
-        df_asset_class_positions = df_asset_class_positions.pivot(index=['Name', 'Ativo Nome Completo'], columns='Portfolio', values='Saldo')
-
-        if selected_visualization == 'Financeiro (R$)':
-          df_asset_class_positions_visualization = df_asset_class_positions
-        elif selected_visualization == 'Percentual da Classe (%)':
-          df_asset_class_positions_visualization = df_asset_class_positions.div(df_asset_class_positions.sum(axis=0), axis=1) * 100
-        elif selected_visualization == 'Percentual do Total (%)':
-          df_asset_class_positions_visualization = df_asset_class_positions.div(df_total_positions_current['Saldo'], axis=1) * 100
-        
-        st.markdown(f"##### {asset_class}")
+      if selected_visualization == 'Financeiro (R$)':
+        df_asset_class_positions_visualization = df_asset_class_positions
+      elif selected_visualization == 'Percentual da Classe (%)':
+        df_asset_class_positions_visualization = df_asset_class_positions.div(df_asset_class_positions.sum(axis=0), axis=1) * 100
+      elif selected_visualization == 'Percentual do Total (%)':
+        df_asset_class_positions_visualization = df_asset_class_positions.div(df_total_positions_current['Saldo'], axis=1) * 100
+      
+      with st.expander(f"{asset_class}", expanded=False):
         st.dataframe(
           style_table(
             df_asset_class_positions_visualization,
@@ -93,5 +108,5 @@ if df is not None:
           )
         )
 
-    except Exception as e:
-      st.error(f"Ocorreu um erro ao carregar os dados: {e}")
+  except Exception as e:
+    st.error(f"Ocorreu um erro ao carregar os dados: {e}")
