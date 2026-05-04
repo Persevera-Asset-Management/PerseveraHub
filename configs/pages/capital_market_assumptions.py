62fc1aa71e44bc2b0a1dafa21e0467cf90d8f1a3
Thales Carmo <thales.carmo@persevera.com.br>
2026-05-04 16:47:59 -0300

    Update Capital Market Assumptions to include additional debt instruments and improve naming consistency
    - Added new entries for 'Debêntures DI', 'Debêntures IPCA', and their respective categories for better clarity.
    - Updated existing entries to enhance naming consistency across the 'Renda Fixa' section.
    - Removed 'Fundos Imobiliários (IFIX)' from the 'Alternativos' category for improved accuracy.

--- configs/pages/capital_market_assumptions.py
+++ configs/pages/capital_market_assumptions.py
@@ -1,14 +1,20 @@
 CAPITAL_MARKET_ASSUMPTIONS = {
     "Renda Fixa": {
         "br_cdi_index": "Caixa e Equivalentes (CDI)",
-        "br_b3_corporate_bond_di_aaa_index": "Renda Fixa Pós-Fixado (Debêntures AAA DI)",
-        "br_b3_corporate_bond_di_ultra_quality_index": "Renda Fixa Pós-Fixado (Debêntures Ultra Quality DI)",
-        "br_b3_corporate_bond_incentivized_ipca_ultra_quality_index": "Renda Fixa IPCA (Debêntures Incentivadas IPCA Ultra Quality)",
-        "br_b3_corporate_bond_incentivized_ipca_aaa_index": "Renda Fixa IPCA (Debêntures Incentivadas IPCA AAA)",
-        "anbima_ima_b5": "Títulos Públicos IPCA Curto (IMA-B 5)",
-        "anbima_ima_b5+": "Títulos Públicos IPCA Longo (IMA-B 5+)",
+
         "anbima_irf_m1": "Títulos Públicos Pré-Fixados Curto (IRF-M 1)",
         "anbima_irf_m1+": "Títulos Públicos Pré-Fixados Longo (IRF-M 1+)",
+        
+        "anbima_ima_b5": "Títulos Públicos IPCA Curto (IMA-B 5)",
+        "anbima_ima_b5+": "Títulos Públicos IPCA Longo (IMA-B 5+)",
+        
+        "anbima_ida_di": "Debêntures DI (IDA DI)",
+        "br_b3_corporate_bond_di_aaa_index": "Debêntures AAA DI (B3)",
+        "br_b3_corporate_bond_di_ultra_quality_index": "Debêntures Ultra Quality DI (B3)",
+        
+        "anbima_ida_ipca": "Debêntures IPCA (IDA IPCA)",
+        "br_b3_corporate_bond_incentivized_ipca_aaa_index": "Debêntures Incentivadas AAA IPCA (B3)",
+        "br_b3_corporate_bond_incentivized_ipca_ultra_quality_index": "Debêntures Incentivadas Ultra Quality IPCA (B3)",
     },
     "Renda Variável": {
         "br_ibovespa": "Bolsa Brasileira (Ibovespa)",
@@ -19,7 +25,6 @@ CAPITAL_MARKET_ASSUMPTIONS = {
     },
     "Alternativos": {
         "anbima_ihfa": "Fundos Multimercado (IHFA)",
-        "br_ifix": "Fundos Imobiliários (IFIX)",
         "gold_100oz_futures": "Ouro (sem variação cambial)",
         # "bitcoin_usd": "Bitcoin",
     },
