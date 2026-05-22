b1dc2bad24d358dd2565378605fe6723e8cf4f50
Thales Carmo <thales.carmo@persevera.com.br>
2026-05-22 11:18:39 -0300

    Update Screener page to include color coding for negative and positive values, enhancing data visualization. This change improves the clarity of displayed metrics by allowing users to easily distinguish between different performance indicators.

--- pages/2_📈_Screener.py
+++ pages/2_📈_Screener.py
@@ -212,7 +212,8 @@ if not raw_data.empty:
         numeric_cols_format_as_int=['ADTV (21d)'],
         numeric_cols_format_as_float=list(data.columns.drop('ADTV (21d)')),
         currency_cols=['ADTV (21d)'],
-        highlight_quartile=['Momentum Score', 'Value Score', 'Liquidity Score', 'Risk Score', 'Quality Score']
+        color_negative_positive_cols=list(data.columns),
+
     )
     st.write(styled_data)
     
